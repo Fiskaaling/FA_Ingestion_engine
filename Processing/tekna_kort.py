51928848d33b58f2107025224b41be57d752d036
Urtapott <johannus@fiskaaling.fo>
2018-09-18 14:58:46 +0100

    plot og scatter frá fílum

--- Processing/tekna_kort.py
+++ Processing/tekna_kort.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import filedialog
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
@@ -36,6 +37,12 @@ def teknakort():
     root = Tk()
     root.geometry("1200x800")
     app = Window(root)
+
+    top = Toplevel()
+    top.wm_attributes('-topmost', 1)
+    top.withdraw()
+    top.protocol('WM_DELETE_WINDOW', top.withdraw)
+
     menu_frame = Frame(app, borderwidth=1, highlightbackground="green", highlightcolor="green", highlightthickness=1)
     menu_frame.pack(fill=X, expand=False, anchor=N)
     content_frame = Frame(app, borderwidth=1, highlightbackground="green", highlightcolor="green", highlightthickness=1)
@@ -57,6 +64,13 @@ def teknakort():
     tekna_btn = Button(menu_frame, text='Tekna Kort', command=lambda: les_og_tekna(text_list.get("1.0", END), fig, canvas)).pack(side=LEFT)
     zoomin_btn = Button(menu_frame, text='+', command=lambda: zoom(0.01, text_list)).pack(side=LEFT)
     zoomout_btn = Button(menu_frame, text='-', command=lambda: zoom(-0.01, text_list)).pack(side=LEFT)
+    teknaLinjur_btn = Button(menu_frame, text='Tekna Linjur', command=lambda: teknaLinjur(text_list, root)).pack(side=LEFT)
+
+
+def teknaLinjur(text_list, root):
+    filnavn = filedialog.askopenfilename(parent=root,title = "Vel Fíl",filetypes = (("csv Fílir","*.csv"),("all files","*.*")))
+    text_list.insert(INSERT, '\nlin_fil=' + filnavn)
+    print('test')
 
 def zoom(mongd, textbox):
     print('zoom ' + str(mongd))
@@ -156,7 +170,14 @@ def les_og_tekna(text, fig, canvas):
                     btn_track = True
             elif variable == 'btn_gridsize':
                 btn_gridsize = command[toindex::]
-
+            elif variable == 'lin_fil':
+                lineData = pd.read_csv(command[toindex::])
+                line_x, line_y = m(lineData['lon'].values, lineData['lat'].values)
+                ax.plot(line_x, line_y, 'b', linewidth=1)
+            elif variable == 'scatter_fil':
+                scatterData = pd.read_csv(command[toindex::])
+                line_x, line_y = m(scatterData['lon'].values, scatterData['lat'].values)
+                ax.scatter(line_x, line_y, 'b', linewidth=1)
         else:
             if command == 'clf':
                 fig.clf()
@@ -176,14 +197,13 @@ def les_og_tekna(text, fig, canvas):
                 fig.colorbar(c)
             elif command == 'btn_contour':
                 lv = np.arange(0, 150, 5)
-                print(lv)
                 c = m.contour(meshgridx, meshgridy, grid_z0, lv, ax=ax)  # Um kodan kiksar her broyt basemap fílin til // har feilurin peik
                 ax.clabel(c, inline=1, fontsize=15, fmt='%2.0f')
-    alioki_lat = [62.273790155, 62.272388430, 62.264901060, 62.266422648]
-    alioki_lon = [-6.727558225, -6.732274219, -6.722096577, -6.717137173]
-    alioki_x, alioki_y = m(alioki_lon, alioki_lat)
-    m.plot(alioki_x, alioki_y, 'b', linewidth=1)
-    ax.plot((alioki_x[0], alioki_x[3]), (alioki_y[0], alioki_y[3]), label='Uppskot til aliøki')
+    #alioki_lat = [62.273790155, 62.272388430, 62.264901060, 62.266422648]
+    #alioki_lon = [-6.727558225, -6.732274219, -6.722096577, -6.717137173]
+    #alioki_x, alioki_y = m(alioki_lon, alioki_lat)
+    #m.plot(alioki_x, alioki_y, 'b', linewidth=1)
+    #ax.plot((alioki_x[0], alioki_x[3]), (alioki_y[0], alioki_y[3]), label='Uppskot til aliøki')
 
     fig.savefig(filnavn + '.png', dpi=int(dpi), bbox_inches='tight')
 
