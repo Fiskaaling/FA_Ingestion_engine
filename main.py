af3994ba4a4c9420d89ff6637e3ce7f8c9e5f694
Urtapott <johannus@fiskaaling.fo>
2018-09-25 14:16:31 +0100

    Byrja uppá ingestion maskinarí

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import Processing.tekna_kort
+import tkinter.ttk as ttk
 
 class Window(Frame):
     def __init__(self, master=None):
@@ -31,6 +32,28 @@ Label(Ingestion_frame, text='Ingestion').pack(side=TOP)
 
 Processing_frame = Frame(app)
 Processing_frame.pack(fill=BOTH, expand=True, side=LEFT, anchor=N)
+
+ingestion_subframe = Frame(Ingestion_frame)
+ingestion_subframe.pack(fill=BOTH, expand=True, side=TOP, anchor=W)
+
+ingestion_listbox = ttk.Treeview(ingestion_subframe)
+scrollbar = Scrollbar(ingestion_subframe, orient=VERTICAL)
+scrollbar.config(command=ingestion_listbox.yview)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+
+
+ingestion_listbox["columns"]=("one","two")
+ingestion_listbox.column("one", width=100 )
+ingestion_listbox.column("two", width=100)
+ingestion_listbox.heading("one", text="coulmn A")
+ingestion_listbox.heading("two", text="column B")
+
+
+#ingestion_listbox.insert(END, 'Test')
+ingestion_listbox.pack(fill=BOTH, expand=True, side=TOP, anchor=W)
+
+
 Label(Processing_frame, text='Processing').pack(side=TOP)
 TeknaKort_Btn = Button(Processing_frame, text='Tekna Kort', command=lambda: Processing.tekna_kort.teknakort())
 TeknaKort_Btn.pack(side=TOP)
