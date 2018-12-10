f914d777813dd37b7c4f3d4ecc74b5e6655110a2
Tróndur <trondurt@fiskaaling.fo>
2018-12-10 16:04:58 +0000

    forksellit

--- misc/geo.py
+++ misc/geo.py
@@ -5,9 +5,3 @@ def round(lat1,lon1,lat2,lon2):
     a = np.cos(np.deg2rad(lat1))
     b = np.cos(np.deg2rad(lat2))
     return r*np.arccos(a*b*(np.cos(np.deg2rad(lon1-lon2))) + np.sin(np.deg2rad(lat1))*np.sin(np.deg2rad(lat2)))
-
-a=[62.2666563333333,62.2712193333333,62.2707601666667,62.2661471666667,62.2666563333333]
-b=[6.71907616666667,6.72514866666667,6.7267795,6.72081416666667,6.71907616666667]
-
-for i in range(len(a)-1):
-    print(round(a[i], b[i], a[i+1], b[i+1]))
