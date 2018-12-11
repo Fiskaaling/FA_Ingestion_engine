5ff9836afa804e5f50829fb0c10896a01f31ddef
Tróndur <trondurt@fiskaaling.fo>
2018-12-11 08:47:22 +0000

    flat

--- misc/geo.py
+++ misc/geo.py
@@ -5,3 +5,9 @@ def round(lat1,lon1,lat2,lon2):
     a = np.cos(np.deg2rad(lat1))
     b = np.cos(np.deg2rad(lat2))
     return r*np.arccos(a*b*(np.cos(np.deg2rad(lon1-lon2))) + np.sin(np.deg2rad(lat1))*np.sin(np.deg2rad(lat2)))
+
+def flat(lat1,lon1,lat2,lon2):
+    '''wrong use round plz'''
+    r = 6371000
+    c = 0.46947156278589086
+    return r*np.sqrt(np.square(np.deg2rad(lat1-lat2))+np.square(np.deg2rad(c*(lon1-lon2))))
