bfff732cada59b30968699586074c3ca792dd8d1
Urtapott <johannus@fiskaaling.fo>
2019-01-28 08:47:20 +0000

    byrja uppá Økið talvu interface

--- FA_DB_Interface/init.py
+++ FA_DB_Interface/init.py
@@ -1,4 +1,5 @@
 import FA_DB_Interface.stovna_geo_okid
+import FA_DB_Interface.stovna_okid
 
 db_host = '192.168.43.94'
 db_user = 'JK'
@@ -6,8 +7,12 @@ db_password = 'koda'
 
 def init(ingestion_listbox):
     databasi = ingestion_listbox.insert("", 0, text='Dátugrunnur')
-    okid = ingestion_listbox.insert(databasi, 0, text="Stovna Geo Økið")
+    ingestion_listbox.insert(databasi, 0, text="Stovna Geo Økið")
+    ingestion_listbox.insert(databasi, 0, text="Stovna Økið")
+
 
 def check_click(item, RightFrame, root):
     if item == 'Stovna Geo Økið':
         FA_DB_Interface.stovna_geo_okid.stovna_geo_okid(RightFrame, root, db_host, db_user, db_password)
+    elif item == 'Stovna Økið':
+        FA_DB_Interface.stovna_okid.stovna_okid(RightFrame, root, db_host, db_user, db_password)
